b2f10b112cdae84925a105b5473483d543df7900
Vladimir123bhdewi <vladimirsolomenin@yandex.ru>
2026-06-18 13:03:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,6 @@ def webhook():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
